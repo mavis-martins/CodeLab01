1b25f75fe16dd1b3b98dd340cd0aa809b9308b3c
Mavis <mavisdeving@gmail.com>
2024-08-19 14:55:32 -0300

    simplificando o js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const heartIcons = document.querySelectorAll('.dateAndFav img');
-    
+
     heartIcons.forEach(icon => {
-        icon.addEventListener('click', function() {
+        icon.addEventListener('click', function () {
             const parentDiv = this.closest('.postCodeLab');
-            const isActive = parentDiv.style.borderColor === 'rgb(224, 123, 103)';
 
-            parentDiv.style.borderColor = isActive ? '' : '#E07B67';
-
+            const isActive = this.src.includes('icon-heart-active.svg');
+            
             if (isActive) {
                 this.src = './assets/icons/icon-heart.svg';
+                parentDiv.style.borderColor = ''; 
             } else {
                 this.src = './assets/icons/icon-heart-active.svg';
+                parentDiv.style.borderColor = '#E07B67';
             }
         });
     });
